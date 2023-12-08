5413a7ea4d541e18a7b2ed8e5a38255d6fb29dbc
WBRK-dev <koertjrweberp@gmail.com>
2023-12-08 13:02:22 +0100

    [UPDATE] .gitignore rules

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const http = require("http");
 const { Server } = require("socket.io");
+require("dotenv").config();
 
 let app = express();
 const server = http.createServer(app);
@@ -12,4 +13,4 @@ app = require(__dirname+"/server.js")(app);
 
 io = require(__dirname+"/server/io/socket.js")(io);
 
-server.listen(8080, () => console.log("Listening"));
+server.listen(process.env.PORT, () => console.log("Listening"));
